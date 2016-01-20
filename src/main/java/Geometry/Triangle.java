25026af65c1df75f370694eb88632878bfa63480
115ek <115ek@users.noreply.github.com>
2016-01-20 10:46:04 +0100

    fixed constructor of Triangle.java

--- src/main/java/Geometry/Triangle.java
+++ src/main/java/Geometry/Triangle.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 
 public  class  Triangle implements Geometry {
     private double length;
-	public Triangle(){
+	public Triangle(double length){
         this.length = length;
     }
     public  double  getArea(){
